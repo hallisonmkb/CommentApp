be2ca93c31cf450f6417fe740ff2bb0553772717
Hallison <Hallison@BRUNA-PC>
2017-03-15 23:00:23 -0300

    Add project files.

---
+++ CommentApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CommentApp.Startup))]
+namespace CommentApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
